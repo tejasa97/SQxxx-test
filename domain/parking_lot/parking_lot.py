d599aef186fafeef61cbaebec998a90f141c8d4a
Tejas Arlimatti <tejasarlimatti@gmail.com>
2021-01-11 11:59:43 +0530

    Added all test cases (edge cases including)

--- domain/parking_lot/parking_lot.py
+++ domain/parking_lot/parking_lot.py
@@ -11,9 +11,6 @@ class ParkingLotManager(metaclass=Singleton):
     class InvalidSlotNumber(Exception):
         ...
 
-    class DoesNotExist(Exception):
-        ...
-
     class SlotEmpty(Exception):
         ...
 
